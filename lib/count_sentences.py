fe39bad946497ed04b9afda1d2534722b7191148
james-mbuvi <james.mbuvi@student.moringaschool.com>
2024-03-02 18:14:50 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = str(value)
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        if not self.value:
+            return 0
+        sentences = self.value.split('.')
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
+      
+    
+
+
